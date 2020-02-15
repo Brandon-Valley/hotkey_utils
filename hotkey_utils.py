951a801d0de01a96055af387fea27d044dd2af58
Brandon Valley <bavvh8@mst.edu>
2020-02-15 15:58:55 -0600

    works

--- hotkey_utils.py
+++ hotkey_utils.py
@@ -34,7 +34,8 @@ def hotkey_pressed():
 def wait_for_hotkey_press(hotkey_str):
     k.add_hotkey(hotkey_str, hotkey_pressed)
     while(keep_waiting_for_hotkey_press):
-        print('waiting')
+#         print('waiting')
+        pass
 
         
         
